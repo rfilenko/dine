7e8aa7eae954e1687380e379ff4340fbec9a1ad3
romanfilenko <filenko@uxf.cz>
2020-06-05 12:46:34 +0200

    added all images, header img, changes to reservations

--- components/Header.js
+++ components/Header.js
@@ -5,13 +5,15 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <Logo />
-        <h2 className="subtitle">Exquisite dining since 1989</h2>
-        <p>
-          Experience our seasonal menu in beautiful country surroundings. Eat
-          the freshest produce from the comfort of our farmhouse.
-        </p>
-        <button className="btn btn--dark">Book a table</button>
+        <div className="hero-content">
+          <Logo />
+          <h2 className="subtitle">Exquisite dining since 1989</h2>
+          <p>
+            Experience our seasonal menu in beautiful country surroundings. Eat
+            the freshest produce from the comfort of our farmhouse.
+          </p>
+          <button className="btn btn--dark">Book a table</button>
+        </div>
       </div>
     </header>
   );
--- components/Reservation.js
+++ components/Reservation.js
@@ -1,8 +1,10 @@
 export default function Reservation() {
   return (
     <section className="reservation">
-      <h3>Ready to make a reservation?</h3>
-      <button className="btn btn--dark">Book a table</button>
+      <div className="container">
+        <h3>Ready to make a reservation?</h3>
+        <button className="btn btn--dark">Book a table</button>
+      </div>
     </section>
   );
 }
