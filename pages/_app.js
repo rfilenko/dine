0e1f5f30411241d85a4b5d0dd20d2bb63eaddfac
romanfilenko <filenko@uxf.cz>
2020-06-05 10:31:48 +0200

    changes in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles.scss'
+import App from 'next/app';
+import React from 'react';
+import Head from 'next/head';
+import '../styles.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <html lang="en" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Dine restaurant landing page, build with nextjs"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="theme-color" content="#111111" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
+  }
 }
-
-export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>
-          <html lang="en" />
-          <meta
-            name="description"
-            content="Dine restaurant landing page, build with nextjs"
-          />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="theme-color" content="#111111" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/images/favicon-32x32.png"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
