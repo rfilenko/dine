4fab1c9a70efa1d641691c63cdb7eb970a5ffbd3
romanfilenko <filenko@uxf.cz>
2020-06-05 10:42:37 +0200

    _document fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from 'next/app';
 import React from 'react';
-import Head from 'next/head';
+
 import '../styles.scss';
 
 export default class MyApp extends App {
@@ -16,22 +16,6 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <>
-        <Head>
-          <html lang="en" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta
-            name="description"
-            content="Dine restaurant landing page, build with nextjs"
-          />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="theme-color" content="#111111" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/images/favicon-32x32.png"
-          />
-        </Head>
         <Component {...pageProps} />
       </>
     );
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,41 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
-      <html>
-        <Head></Head>
+      <Html>
+        <Head>
+          <html lang="en" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+            key="viewport"
+          />
+          <meta
+            name="description"
+            content="Dine restaurant landing page, build with nextjs"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="theme-color" content="#111111" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
+
+export default MyDocument;
