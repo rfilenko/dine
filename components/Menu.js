6d538510d3a4e8a018026b171c6e461a413b6cdf
romanfilenko <filenko@uxf.cz>
2020-06-12 14:53:52 +0200

    menuItems images proper tags

--- components/Menu.js
+++ components/Menu.js
@@ -17,22 +17,7 @@ export default function Menu() {
         </div>
         <ul className="menu-list">
           {menuList.map((item) => (
-            <MenuItem item={item} />
-            // <li className="menu-item" key={item.id}>
-            //   <div className="menu-item__img">
-            //     <img
-            //       srcSet={`/images/${item.imgName}-mobile.jpg 327w,
-            //    /images/${item.imgName}-desktop-tablet.jpg 128w"
-            //       sizes="(max-width: 499px) 327px, 128px"
-            //       src="/images/${item.imgName}-mobile.jpg"
-            //       alt=${item.title}`}
-            //     />
-            //   </div>
-            //   <div className="menu-item__content">
-            //     <h4 className="menu-item__title">{item.title}</h4>
-            //     <p className="menu-item__desc">{item.description}</p>
-            //   </div>
-            // </li>
+            <MenuItem item={item} key={item.id} />
           ))}
         </ul>
       </div>
--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,13 +1,13 @@
 const MenuItem = ({ item }) => {
   return (
-    <li className="menu-item" key={item.id}>
+    <li className="menu-item">
       <div className="menu-item__img">
         <img
           srcSet={`/images/${item.imgName}-mobile.jpg 327w,
-               /images/${item.imgName}-desktop-tablet.jpg 128w"
-                  sizes="(max-width: 499px) 327px, 128px"
-                  src="/images/${item.imgName}-mobile.jpg"
-                  alt=${item.title}`}
+               /images/${item.imgName}-desktop-tablet.jpg 128w`}
+          sizes="(max-width: 499px) 327px, 128px"
+          src={`/images/${item.imgName}-mobile.jpg`}
+          alt={`${item.title}`}
         />
       </div>
       <div className="menu-item__content">
