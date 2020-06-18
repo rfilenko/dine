3dbe31f7f35ce8a85922efc326ece7dbec3b879c
romanfilenko <filenko@uxf.cz>
2020-06-18 14:31:51 +0200

    form inputs validation

--- components/ReservationForm.js
+++ components/ReservationForm.js
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import Router from 'next/router';
 
 const ReservationForm = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data) => {
     console.log(data);
     setTimeout(() => {
@@ -11,23 +11,39 @@ const ReservationForm = () => {
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="name">Enter your name</label>
-      <input ref={register} type="text" name="name" placeholder="John Apples" />
+      <div className="form-group" className={errors.name && 'form-group error'}>
+        <label htmlFor="name">Enter your name</label>
+        <input
+          ref={register({ required: true, minLength: 2 })}
+          type="text"
+          name="name"
+          placeholder="John Apples"
+        />
+        {errors.name && <p className="form-error">This field is required</p>}
+      </div>
+      <div
+        className="form-group"
+        className={errors.email && 'form-group error'}
+      >
+        <label htmlFor="email">Enter email</label>
+        <input
+          ref={register({ required: true })}
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        {errors.email && <p className="form-error">This field is required</p>}
+      </div>
 
-      <label htmlFor="email">Enter email</label>
-      <input ref={register} type="email" name="email" placeholder="Email" />
-
-      <label htmlFor="people-count" defaultValue="2">
-        How many people
-      </label>
-      <select ref={register} name="people-count">
+      <label htmlFor="peopleCount">How many people</label>
+      <select ref={register} name="peopleCount" defaultValue="2">
         <option value="1">1 person</option>
         <option value="2">2 persons</option>
         <option value="3">3 people</option>
         <option value="4">4 people</option>
       </select>
 
-      <button type="submit" className="btn btn--outline">
+      <button type="submit" className="btn btn--black">
         MAKE RESERVATION
       </button>
     </form>
