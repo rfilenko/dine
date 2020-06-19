7ea0a2cda747ce86465eeebcfcd99ca5f202981c
romanfilenko <filenko@uxf.cz>
2020-06-19 13:49:09 +0200

    added modal with reservation info

---
+++ components/ModalReservation.js
@@ -0,0 +1,44 @@
+import Modal from 'react-modal';
+Modal.setAppElement('.address');
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    maxHeight: '300px',
+    width: '90%',
+    maxWidth: '575x',
+    overflow: 'hidden',
+    background: '#ffffff',
+    borderRadius: 'none',
+    padding: '2rem',
+    transform: 'translate(-50%, -50%)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: '0 75px 100px -50px #384255',
+    backgroundColor: '#111111',
+    color: '#ffffff',
+    backgroundImage: `url(/images/hero-bg-mobile.jpg)`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'top',
+    backgroundSize: 'contain',
+    border: 'none',
+  },
+};
+const ModalReservation = ({ personName, modalIsOpen }) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      style={customStyles}
+      contentLabel="Confirmation Modal"
+    >
+      <p>
+        Thank you for your reservation, {personName.split(' ')[0]} 🙂. <br />{' '}
+        You will be redirected to Home Page
+      </p>
+    </Modal>
+  );
+};
+
+export default ModalReservation;
--- components/ReservationForm.js
+++ components/ReservationForm.js
@@ -1,59 +1,67 @@
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 import Router from 'next/router';
+import { useForm } from 'react-hook-form';
+import ModalReservation from './ModalReservation';
 
 const ReservationForm = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [personName, setPersonName] = useState('');
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data) => {
-    console.log(data);
+    setModalIsOpen(true);
+    setPersonName(data.name);
     setTimeout(() => {
       Router.push('/');
-    }, 2000);
+    }, 5000);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={errors.name ? 'form-group error' : 'form-group'}>
-        <label htmlFor="name">
-          <span className="sr-only">Enter your name</span>
-          <input
-            ref={register({ required: true, minLength: 2 })}
-            type="text"
-            name="name"
-            placeholder="John Apples"
-          />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={errors.name ? 'form-group error' : 'form-group'}>
+          <label htmlFor="name">
+            <span className="sr-only">Enter your name</span>
+            <input
+              ref={register({ required: true })}
+              type="text"
+              name="name"
+              placeholder="John Apples"
+            />
 
-          {errors.name && <p className="form-error">This field is required</p>}
-        </label>
-      </div>
-      <div className={errors.email ? 'form-group error' : 'form-group'}>
-        <label htmlFor="email">
-          <span className="sr-only">Enter email</span>
-          <input
-            ref={register({ required: true })}
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
+            {errors.name && <p className="form-error">Name is required</p>}
+          </label>
+        </div>
+        <div className={errors.email ? 'form-group error' : 'form-group'}>
+          <label htmlFor="email">
+            <span className="sr-only">Enter email</span>
+            <input
+              ref={register({ required: true })}
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
 
-          {errors.email && <p className="form-error">This field is required</p>}
-        </label>
-      </div>
+            {errors.email && <p className="form-error">Email is required</p>}
+          </label>
+        </div>
 
-      <div className={errors.peopleCount ? 'form-group error' : 'form-group'}>
-        <label htmlFor="peopleCount">
-          <span className="sr-only"> How many people</span>
-          <select ref={register} name="peopleCount" defaultValue="2">
-            <option value="1">1 person</option>
-            <option value="2">2 persons</option>
-            <option value="3">3 people</option>
-            <option value="4">4 people</option>
-          </select>
-        </label>
-      </div>
+        <div className={errors.peopleCount ? 'form-group error' : 'form-group'}>
+          <label htmlFor="peopleCount">
+            <span className="sr-only"> How many people</span>
+            <select ref={register} name="peopleCount" defaultValue="2">
+              <option value="1">1 person</option>
+              <option value="2">2 persons</option>
+              <option value="3">3 people</option>
+              <option value="4">4 people</option>
+            </select>
+          </label>
+        </div>
 
-      <button type="submit" className="btn btn--black">
-        MAKE RESERVATION
-      </button>
-    </form>
+        <button type="submit" className="btn btn--black">
+          MAKE RESERVATION
+        </button>
+      </form>
+      <ModalReservation modalIsOpen={modalIsOpen} personName={personName} />
+    </>
   );
 };
 
